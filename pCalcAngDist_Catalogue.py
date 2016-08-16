7d7b2be3fd5714897a0a280b5a2be0f819ebe21d
mitsunari <takhs.3tnri@gmail.com>
2016-08-16 04:35:00 +0100

    Correct pCalcAngDist_Catalogue.py

--- pCalcAngDist_Catalogue.py
+++ pCalcAngDist_Catalogue.py
@@ -12,27 +12,40 @@ from math import sin, cos, asin, acos, pi
 import numpy as np
 
 
-def vecterSkyPosition(aPosiRad):
+def vecterSkyPosition(aPosiDeg):
     """Return a 3D vector (NumPy array) corresponding to the given (RA, DEC)
-vecterSkyPosition(RA in radians, DEC in radians)
+vecterSkyPosition(RA in radians, DEC in degrees)
 """
-    vecPosi = np.array([cos(aPosiRad[0])*sin(pi/2.-aPosiRad[1]), sin(aPosiRad[0])*sin(pi/2.-aPosiRad[1]), sin(pi/2.-aPosiRad[1])])
-    return vecterSkyPosition
+    npaPosiDeg = np.array(aPosiDeg)
+    aPosiRad = npaPosiDeg * math.radians(1)
+    cosPhi = cos(aPosiRad[0])
+    cosTh = cos(pi/2.-aPosiRad[1])
+    sinPhi = sin(aPosiRad[0])
+    sinTh = sin(pi/2.-aPosiRad[1])
+    vecPosi = np.array([cosPhi*sinTh, sinPhi*sinTh, cosTh])
+    for iel in range(len(vecPosi)):
+        if abs(vecPosi[iel])<1e-15:
+            vecPosi[iel] = 0.
+    return vecPosi
 
 
-def calcAngDist_Catalogue(aPosiDeg, fitsCatalogue):
+def calcAngDist_Catalogue(aPosiDeg, fitsCatalogue, nSrc=0):
     """Calculate angular distance between a sky position and evergy source in a catalogue. Return an array of angular distance in radians.
 calcAngDist_Catalogue([RA in deg, DEC in deg], 'Path of your catalogue')
 """
     HDULIST = fits.open(fitsCatalogue)
     TBDATA = HDULIST[1].data
     npaPosiDeg = np.array(aPosiDeg)
-    A_POSI_RAD = npaPosiDeg * math.radians(1)
-    VEC_POSI = vecterSkyPosition(A_POSI_RAD)
-    NSRC = len(TBDATA['Source_Name'])
+    VEC_POSI = vecterSkyPosition(aPosiDeg)
+    print "Given sky position vector:", VEC_POSI
+    if nSrc==0:
+        nSrc = len(TBDATA['Source_Name'])
     aAngDist = np.array([])
-    for iSrc in range(NSRC):
-        vecSrc = vecterSkyPosition([TBDATA['RAJ2000'][iSrc], TBDATA['DECJ2000'][iSrc]])
-        angRad = acos(np.dot(VEC_POSI, vecSrc))
-        aAngDist.append(angRad)
-    return aAngDist
+    for iSrc in range(nSrc):
+        vecSrc = vecterSkyPosition([TBDATA['RAJ2000'][iSrc], TBDATA['DEJ2000'][iSrc]])
+        prodDot = np.dot(VEC_POSI, vecSrc)
+        angRad = acos(prodDot)
+        aAngDist = np.append(aAngDist, angRad)
+        if math.degrees(angRad)<15:
+            print TBDATA['Source_Name'][iSrc], "(", TBDATA['ASSOC'][iSrc], ") :", math.degrees(angRad), "deg, Flux50=", TBDATA['Flux50'][iSrc]
+    return [TBDATA['Source_Name'][:nSrc], aAngDist]
