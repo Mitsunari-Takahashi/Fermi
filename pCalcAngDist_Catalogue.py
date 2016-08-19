8864bfc7320c57daeede5e302840717b1ff32b4f
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2016-08-19 08:35:23 -0700

    Add pFindHEALPix.py

--- pCalcAngDist_Catalogue.py
+++ pCalcAngDist_Catalogue.py
@@ -3,13 +3,18 @@
 """
 import sys
 from astropy.io import fits
-#from astropy.coordinates import ICRS, Galactic, FK4, FK5  # Low-level frames
-#from astropy.coordinates import Angle, Latitude, Longitude  # Angles
-#import astropy.units as u
 from array import array
 import math
 from math import sin, cos, asin, acos, pi
 import numpy as np
+
+
+def suppress_vector_too_small_compo(vecPosi):
+    vecSupp = vecPosi.copy()
+    for iel in range(len(vecPosi)):
+        if abs(vecPosi[iel])<1e-15:
+            vecSupp[iel] = 0.
+    return vecSupp
 
 
 def vecterSkyPosition(aPosiDeg):
@@ -23,14 +28,12 @@ vecterSkyPosition(RA in radians, DEC in degrees)
     sinPhi = sin(aPosiRad[0])
     sinTh = sin(pi/2.-aPosiRad[1])
     vecPosi = np.array([cosPhi*sinTh, sinPhi*sinTh, cosTh])
-    for iel in range(len(vecPosi)):
-        if abs(vecPosi[iel])<1e-15:
-            vecPosi[iel] = 0.
-    return vecPosi
+    vecRslt = suppress_vector_too_small_compo(vecPosi)
+    return vecRslt
 
 
-def calcAngDist_Catalogue(aPosiDeg, fitsCatalogue, nSrc=0):
-    """Calculate angular distance between a sky position and evergy source in a catalogue. Return an array of angular distance in radians.
+def calcAngDist_Catalogue(aPosiDeg, fitsCatalogue, threshold=0, ang_print=0, nSrc=0):
+    """Calculate angular distance between a sky position and evergy source in a catalogue. Return an array of dictionaries which have "Source_Name", "ASSOC", "ANG_DIST" in degrees, "Flux50".
 calcAngDist_Catalogue([RA in deg, DEC in deg], 'Path of your catalogue')
 """
     HDULIST = fits.open(fitsCatalogue)
@@ -38,14 +41,22 @@ calcAngDist_Catalogue([RA in deg, DEC in deg], 'Path of your catalogue')
     npaPosiDeg = np.array(aPosiDeg)
     VEC_POSI = vecterSkyPosition(aPosiDeg)
     print "Given sky position vector:", VEC_POSI
+    aDictResults = []
     if nSrc==0:
         nSrc = len(TBDATA['Source_Name'])
-    aAngDist = np.array([])
     for iSrc in range(nSrc):
         vecSrc = vecterSkyPosition([TBDATA['RAJ2000'][iSrc], TBDATA['DEJ2000'][iSrc]])
-        prodDot = np.dot(VEC_POSI, vecSrc)
-        angRad = acos(prodDot)
-        aAngDist = np.append(aAngDist, angRad)
-        if math.degrees(angRad)<15:
-            print TBDATA['Source_Name'][iSrc], "(", TBDATA['ASSOC'][iSrc], ") :", math.degrees(angRad), "deg, Flux50=", TBDATA['Flux50'][iSrc]
-    return [TBDATA['Source_Name'][:nSrc], aAngDist]
+        angRad = get_ang_dist_vectors(VEC_POSI, vecSrc)
+        if TBDATA['Flux50'][iSrc]>=threshold:
+            aDictResults.append({"Source_Name":TBDATA['Source_Name'][iSrc], "ASSOC":TBDATA['ASSOC'][iSrc], "ANG_DIST":math.degrees(angRad), "Flux50":TBDATA['Flux50'][iSrc]})
+            if math.degrees(angRad)<ang_print:
+                print aDictResults[-1]
+    return aDictResults
+
+
+def get_ang_dist_vectors(vec0, vec1):
+    """Return the angular distance between two vectors (should be normalized) in radians.
+"""
+    prodDot = np.dot(vec0, vec1)
+    return acos(prodDot)
+    
---
+++ pFindHEALPix.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+import math
+from math import pi, degrees, radians
+import ROOT
+from ROOT import TTree
+import numpy as np
+from array import array
+from astropy.io import fits
+from astropy.coordinates import SkyCoord  # High-level coordinates
+from astropy.coordinates import ICRS, Galactic, FK4, FK5  # Low-level frames
+from astropy.coordinates import Angle, Latitude, Longitude  # Angles
+import astropy.units as u
+import healpy as hp
+from healpy import pixelfunc as hppf
+from pConvertSkyCoords import *
+from pCalcAngDist_Catalogue import *
+
+
+def find_galoff_healpxs(nhpside=16, threshold_flux50=0, path_catalogue_excl='/nfs/farm/g/glast/u/mtakahas/data/catalogue/gll_psch_v09.fit'):
+    """Returns a list of HEALPiX pixles which satisfy your conditions as OFF region of Galactic ridge. The coordinate system is RADEC.
+"""
+    NPIX = hppf.nside2npix(nhpside)
+    MAX_PIX_DEG = degrees(hppf.max_pixrad(nhpside))
+    ar_pix_incl = []
+    for ipix in range(NPIX):
+        theta, phi = hppf.pix2ang(nhpside, ipix)
+        radec_rad = [pi/2.-theta, phi]
+        radec_deg = [degrees(radec_rad[0]), degrees(radec_rad[1])]
+        gal_deg = convert_radec_to_galactic(radec_deg)
+        if abs(gal_deg[1])>=50 && 90<=gal_deg[0]<270:
+            ar_ang_dist = calcAngDist_Catalogue(radec_deg, path_catalogue_excl, threshold_flux50)
+            for di in ar_ang_dict:
+                if di['ANG_DIST']<=MAX_PIX_DEG:
+                    break
+            else:
+                ar_pix_incl.append(ipix)
+    return ar_pix_incl
+
+    
+def find_pointon_healpxs(ra_poi, dec_poi, ang_cut, nhpside=512):
+    NPIX = hppf.nside2npix(nhpside)
+    MAX_PIX_DEG = degrees(hppf.max_pixrad(nhpside))
+    RADEC_POI = [radians(ra_poi), radians(dec_poi)]
+    VEC_POI = hppf.ang2vec(RADEC_POI[0], RADEC_POI[1])
+    ar_pix_incl = []
+    for ipix in range(NPIX):
+        vec = pix2vec(nhpside, ipix)
+        ang_dist = get_ang_dist_vectors(VEC_POI, vec)
+        if degrees(ang_dist)<ang_cut:
+            ar_pix_inc.append(ipix)
+    return ar_pix_inc
+    
+
+    
+    
+    
