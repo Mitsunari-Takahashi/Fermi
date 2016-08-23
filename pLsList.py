b56da12156901a479fce458916de1e52484463f1
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2016-08-22 23:03:22 -0700

    Correct two bugs, command and importing commands module

--- pLsList.py
+++ pLsList.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-import command
+import commands
 import subprocess
 
 def ls_list(path_files):
     """Returns the results of 'ls <path_files>' command in list format.
     """
-    ret_ls = commands.getoutput("{0}".format(path_files))
+    print path_files
+    ret_ls = commands.getoutput("ls {0}".format(path_files))
     li_path_files = ret_ls.split("\n")
+    print li_path_files
     return li_path_files
