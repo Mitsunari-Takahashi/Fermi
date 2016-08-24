7eb063f59016894464ba9001828df09344dba440
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2016-08-25 00:11:46 +0900

    Looking for the cause of strange DEC around the GRB duration

--- pLivetime.py
+++ pLivetime.py
@@ -12,7 +12,7 @@ from astropy.coordinates import ICRS, Galactic, FK4, FK5  # Low-level frames
 from astropy.coordinates import Angle, Latitude, Longitude  # Angles
 import astropy.units as u
 #import healpy as hp
-#from healpy import pixelfunc as hppf
+from healpy import pixelfunc as hppf
 #sys.path.append("/disk/gamma/cta/store/takhsm/FermiMVA/AllSky")
 #sys.path.append("/home/takhsm/FermiMVA/python")
 ROOT.gROOT.SetBatch()
@@ -48,8 +48,10 @@ def make_livetime_histogram(aHtgLt, nRegion, pathFileScAll, metStart, metStop, a
         aSTART, aSTOP = tbdataSC.field('START'), tbdataSC.field('STOP')
         aRA_ZENITH = tbdataSC.field('RA_ZENITH')
         aDEC_ZENITH = tbdataSC.field('DEC_ZENITH')
-        aRA_SCZ, aRA_SCX = tbdataSC.field('RA_SCZ'), tbdataSC.field('RA_SCX')
-        aDEC_SCZ, aDEC_SCX = tbdataSC.field('DEC_SCZ'), tbdataSC.field('DEC_SCX')
+        aRA_SCZ = tbdataSC.field('RA_SCZ')
+        aRA_SCX = tbdataSC.field('RA_SCX')
+        aDEC_SCZ = tbdataSC.field('DEC_SCZ')
+        aDEC_SCX = tbdataSC.field('DEC_SCX')
         aLIVETIME = tbdataSC.field('LIVETIME')
         nTI = len(aSTART)
         print "  ", fileToI, "(", nTI, "intervals )"
@@ -58,13 +60,23 @@ def make_livetime_histogram(aHtgLt, nRegion, pathFileScAll, metStart, metStop, a
                 tti = aLIVETIME[iTI]
                 coordsSCZ = SkyCoord(aRA_SCZ[iTI], aDEC_SCZ[iTI], unit="deg")
                 coordsZenith = SkyCoord(aRA_ZENITH[iTI], aDEC_ZENITH[iTI], unit="deg")
+                tplot = (aSTART[iTI]+aSTOP[iTI])/2.-origin_time
+                vecSCX = np.array(hppf.ang2vec(math.pi/2.-math.radians(aDEC_SCX[iTI]), math.radians(aRA_SCX[iTI])))
+                vecSCZ = np.array(hppf.ang2vec(math.pi/2.-math.radians(aDEC_SCZ[iTI]), math.radians(aRA_SCZ[iTI])))
+                vecSCY = np.cross(vecSCZ, vecSCX)
+                aAngSCY = hppf.vec2ang(vecSCY)
+                coordsSCY = SkyCoord(math.degrees(aAngSCY[1]), math.degrees(math.pi/2.-aAngSCY[0]), unit="deg")
                 for iR in range(nRegion):
                     for (jpix, coordsPix) in enumerate(aCoordsPix_array[iR]):
                         angSCZ = coordsSCZ.separation(coordsPix)
                         radSCZ = float(angSCZ.to_string(unit=u.rad, decimal=True))
+                        angSCY = coordsSCY.separation(coordsPix)
+                        radSCY = float(angSCY.to_string(unit=u.rad, decimal=True))
                         angZenith = coordsZenith.separation(coordsPix)
                         degZenith = float(angZenith.to_string(unit=u.deg, decimal=True))
-                        aHtgLt[iR].Fill(cos(radSCZ), degZenith, (aSTART[iTI]+aSTOP[iTI])/2.-origin_time, tti*aAreaPix_array[iR][jpix])
+                     #   aHtgLt[iR].Fill(cos(radSCZ), degZenith, (aSTART[iTI]+aSTOP[iTI])/2.-origin_time, tti*aAreaPix_array[iR][jpix])
+                        aHtgLt[iR].Fill(cos(radSCZ), degZenith, tplot, tti*aAreaPix_array[iR][jpix])
+                print iTI, aSTART[iTI], aRA_SCZ[iTI], aDEC_SCZ[iTI], tbdataSC.field('LAT_MODE')[iTI]#math.degrees(aAngSCY[1]), math.degrees(math.pi/2.-aAngSCY[0]), degZenith, math.degrees(radSCY)
             #if iTI%1==0:
              #   print iTI
                 #rate = int((aSTOP[iTI]-metStart)/(metStop-metStart)*100.+0.5)
@@ -78,5 +90,8 @@ def make_livetime_histogram(aHtgLt, nRegion, pathFileScAll, metStart, metStop, a
                 #else:
                  #   meter = "\r[{0}{1}]".format("=" * rate, ' ' * (100-rate))
                 #sys.stdout.write(meter
-                sys.stdout.write("\r{0}".format(iTI))
+                #sys.stdout.write("\r{0}".format(tplot))
+                #sys.stdout.write("coordsPix: {0}\n".format(coordsPix))
+                #sys.stdout.write("angSCZ: {0}\n".format(angSCZ))
+                #sys.stdout.write("angZenith: {0}\n\n".format(angZenith))
                 sys.stdout.flush()
