8e43f61b20c5b9b14c5dda96fe38a8320c1888b2
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-01-20 18:04:45 +0900

    Now can use imported ExtrapolateFlux

--- ExtrapolateFlux.py
+++ ExtrapolateFlux.py
@@ -4,13 +4,8 @@ import ROOT
 import click
 ROOT.gROOT.SetBatch()
 
-@click.command()
-@click.argument('energy', type=float)
-@click.argument('dnde', type=float)
-@click.option('--pwlindex', type=float, default=2.)
-def main(energy, dnde, pwlindex):
-    """Extrapolates the Power-law function of dN/dE and returns an array of integral flux.
-"""
+
+def ExtrapolateFlux(energy, dnde, pwlindex):
     fc_dnde = ROOT.TF1('fc_dnde_pwl', '[0]*(x/[1])**(-[2])', 100, 1000000) # x = Energy in MeV (linear)
     fc_dnde.FixParameter(0, dnde)
     fc_dnde.FixParameter(1, energy)
@@ -24,6 +19,17 @@ def main(energy, dnde, pwlindex):
 
     for (ibin, edgelow) in enumerate(LST_BINEDGE_LOW):
         lst_flux_itgl.append(fc_dnde.Integral(10**edgelow, 10**(edgelow+BINW)))
+    return lst_flux_itgl
+
+
+@click.command()
+@click.argument('energy', type=float)
+@click.argument('dnde', type=float)
+@click.option('--pwlindex', type=float, default=2.)
+def main(energy, dnde, pwlindex):
+    """Extrapolates the Power-law function of dN/dE and returns an array of integral flux.
+"""
+    lst_flux_itgl = ExtrapolateFlux(energy, dnde, pwlindex)
     print lst_flux_itgl
 
 
