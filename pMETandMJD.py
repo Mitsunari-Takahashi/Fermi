7471cd0adc31634ac727be4bca00f3ac6844ccbe
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-07-30 11:44:33 -0700

    Leap time

--- pMETandMJD.py
+++ pMETandMJD.py
@@ -1,11 +1,40 @@
+#!/usr/bin/env python
+
+import click
+
+DateOffset = 51910.0
+SecPerDay = 86400.
+
 def ConvertMetToMjd(MET):
-   DateOffset=51910.0
-   MJD=MET/3600./24.+DateOffset
+   #Correction for leap seconds
+   nleapsec = 0
+   if MET > 157766400: #2006-01-01 00:00:00.000
+      nleapsec+=1
+   if MET > 252460801.0: #2009-01-01 00:00:00.000
+      nleapsec+=1
+   if MET > 362793602.0: #2012-07-01 00:00:00.000
+      nleapsec+=1
+   if MET > 457401603.0: #2015-07-01 00:00:00.000
+      nleapsec+=1
+   if MET > 504921604.0: #2017-01-01 00:00:00
+      nleapsec+=1
+   MJD=(MET-nleapsec)/SecPerDay+DateOffset
    return MJD
 
 def ConvertMjdToMet(MJD):
-   DateOffset=51910.0
-   MET=(MJD-DateOffset)*24.*3600.
+   #Correction for leap seconds
+   nleapsec = 0
+   if MJD > 53736.0: #2006-01-01 00:00:00.000
+      nleapsec+=1
+   if MJD > 54832.0: #2009-01-01 00:00:00.000
+      nleapsec+=1
+   if MJD > 56109.0: #2012-07-01 00:00:00.000
+      nleapsec+=1
+   if MJD > 57204.0: #2015-07-01 00:00:00.000
+      nleapsec+=1
+   if MJD > 57754.0: #2017-01-01 00:00:00.000
+      nleapsec+=1
+   MET = (MJD-DateOffset) * SecPerDay + nleapsec
    return MET
 
 def ConvertMetToFMW(MET):
@@ -14,3 +43,18 @@ def ConvertMetToFMW(MET):
    MJD=ConvertMetToMjd(MET)
    FMW=(MJD-DateOffset)/7.+WeekOffset
    return FMW
+
+
+@click.command()
+@click.argument('source', type=float)
+@click.option('--mode', '-m', type=click.Choice(['MJDtoMET', 'METtoMJD']))
+def main(source, mode):
+   if mode=='MJDtoMET':
+      result = ConvertMjdToMet(source)
+   elif mode=='METtoMJD':
+      result = ConvertMetToMjd(source)  
+   print result
+
+
+if __name__ == '__main__':
+    main()
