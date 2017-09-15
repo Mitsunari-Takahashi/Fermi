3dd8e7923f9a3171d26ae2ac7aeefb1a86541acd
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-09-15 02:16:21 -0700

    Setting of spectral type and parameters

--- STLikelihoodAnalysis/UnbinnedGRBAnalysis.py
+++ STLikelihoodAnalysis/UnbinnedGRBAnalysis.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python
 
+import sys
 import click
 import pLATLikelihoodConfig
-#import pyLikelihood
-#from UnbinnedAnalysis import *
 from STLikelihoodAnalysis import get_module_logger
 
 ##### Logger #####
 logger = get_module_logger(__name__)
 
 
-def unbinned_grb_analysis(name, mode, force, suffix, grbcatalogue):
-    #self.obs = UnbinnedObs()#self.path_filtered_gti, self.path_ft2, expMap=self.path_exposure, expCube=self.path_livetime, irfs=self.irfs)
-    #self.like = UnbinnedAnalysis() #self.obs, self.path_model_xml, optimizer='NewMinuit')
-    grb = pLATLikelihoodConfig.GRBTarget(name, grbcatalogue)
-    ana = pLATLikelihoodConfig.GRBConfig(grb, mode, emin=100., emax=5623.41, emin_eval=10000., emax_eval=100000.)
-    ana.setup(force={'download':False, 'filter':False, 'maketime':False, 'livetime':False, 'exposure':False, 'model_3FGL_sources':False, 'diffuse_responses':False})
-    # ana.set_directories()
-    # ana.download()
-    # ana.filter()
-    # ana.maketime()
-    # ana.livetime(force)
-    # ana.exposure(force)
-    # ana.model_3FGL_sources(force)
-    # ana.diffuse_responses(force)
-    ana.fit(bredo=False)
+def unbinned_grb_analysis(name, mode, emin, emax, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, outdir, masifps):
+    if spectraltype=='PowerLaw':
+        spectralpars = {'Prefactor':1e-10, 'Index':-2.0, 'Scale':1000.}
+    elif spectraltype=='ExpCutoff':
+        spectralpars = {'Prefactor':1e-10, 'Index':-2.0, 'Scale':1000., 'Ebreak':10.0, 'P1':10000., 'P2':0, 'P3':0}
+    else:
+        logger.critical("""{0} is NOT available!!! Use PowerLaw or ExpCutoff.""".format(spectraltype))
+        sys.exit(1)
+    grb = pLATLikelihoodConfig.GRBTarget(name, grbcatalogue, spectraltype=spectraltype, spectralpars=spectralpars)
+    ana = pLATLikelihoodConfig.GRBConfig(grb, mode, emin=emin, emax=emax, deg_roi=roi, psForce=masifps)
+    ana.setup(force={'download':False, 'filter':force, 'maketime':force, 'livetime':force, 'exposure':force, 'model_3FGL_sources':True, 'diffuse_responses':force})
+    if modelonly==True:
+        esl = ana.set_likelihood()
+        sys.exit(esl)
+    ana.fit(bredo=True)
     ana.plot_countspectra_fitted()
     ana.eval_flux_and_error()
 
 
 @click.command()
 @click.argument('name', type=str)
-#@click.argument('dst', nargs=-1)
-@click.option('--suffix', type=str, default='')
-@click.option('--grbcatalogue', type=str, default=pLATLikelihoodConfig.GRB_CATALOGUE_LTF)
-#@click.option('--values', multiple=True)
-@click.option('--mode', type=click.Choice(['unified', 'prompt', 'afterglow', 'earlyAG', 'lateAG', 'lightcurve', 'special']))
+@click.option('--grbcatalogue', '-c', type=str, default=pLATLikelihoodConfig.GRB_CATALOGUE_LTF)
+@click.option('--mode', '-m', type=click.Choice(['unified', 'prompt', 'afterglow', 'earlyAG', 'lateAG', 'lightcurve', 'special']))
+@click.option('--emin', type=float, default=100.)
+@click.option('--emax', type=float, default=100000.)
+@click.option('--roi', type=float, default=12.)
+@click.option('--spectraltype', type=click.Choice(['PowerLaw', 'ExpCutoff']))
+@click.option('--suffix', '-s', type=str, default='')
 @click.option('--force', '-f', is_flag=True)
-def main(name, mode, force, suffix, grbcatalogue):
-    unbinned_grb_analysis(name, mode, force, suffix, grbcatalogue)
+@click.option('--modelonly', is_flag=True)
+@click.option('--masifps', is_flag=True)
+@click.option('--refit', '-r', is_flag=True)
+@click.option('--outdir', '-o', type=str, default='')
+def main(name, mode, emin, emax, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, outdir, masifps):
+    unbinned_grb_analysis(name, mode, emin, emax, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, outdir, masifps)
 
 
 if __name__ == '__main__':
