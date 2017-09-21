2bf534a7b214f31c864d4b6f825e8ccb4aa04c07
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-09-21 02:37:06 -0700

    FindGoodstatPeriods.py: introduced get_entries_roi

--- FindGoodstatPeriods.py
+++ FindGoodstatPeriods.py
@@ -4,6 +4,9 @@ import sys
 import numpy as np
 import click
 from astropy.io import fits
+from astropy.coordinates import SkyCoord  # High-level coordinates
+from astropy.coordinates import ICRS, Galactic, FK4, FK5  # Low-level frames
+from astropy.coordinates import Angle, Latitude, Longitude  # Angles
 import astropy.units as u
 from math import sqrt
 import commands
@@ -22,34 +25,77 @@ def get_entries(pathFileEvt, metStart, metStop):
     return n
 
 
-def find_goodstat_periods(pathFileEvt, metStart, metStop, nthreshold, noffset=0): #, ethreshold=100., evtclass=128):
-    """Look over spacecraft files and find time intervals which have more than N events.
+def get_entries_roi(pathFileEvt, tStart, tStop, rlim=0.0, ra=0, dec=0, torigin=0.0):
+    """Return entry number between tStart - tStop from torigin, and within rlim degrees from (ra, dec).
 """
-    periods_goodstat = [metStart]
-    nstat = 0
+    metStart = tStart + torigin
+    metStop = tStop + torigin
     hdulistEVT = fits.open(pathFileEvt)
     tbdataEVT = hdulistEVT[1].data
     aTIME = tbdataEVT.field('TIME')
-    #aENERGY = tbdataEVT.field('ENERGY')
-    #aEVTCLASS = tbdataEVT.field('EVENT_CLASS')
-    aTIME.sort()
+    aRA = tbdataEVT.field('RA')
+    aDEC = tbdataEVT.field('DEC')
     nEVT = len(aTIME)
     time_prev = 0
+    coords_target = SkyCoord(ra, dec, unit="deg")
+    nstat = 0
+    for iEVT in range(1, nEVT):
+        if aTIME[iEVT]<time_prev:
+            print 'Odd order!!!'
+            sys.exit(1)
+        if aTIME[iEVT]>=metStart and aTIME[iEVT]<metStop:
+            coords_event = SkyCoord(aRA[iEVT], aDEC[iEVT], unit="deg")
+            sep_ang = coords_target.separation(coords_event)
+            sep_deg = float(sep_ang.to_string(unit=u.deg, decimal=True))
+            if sep_deg<=rlim:
+                nstat+=1
+        time_prev = aTIME[iEVT]
+    return nstat
+
+
+def find_goodstat_periods(pathFileEvt, tStart, tStop, nthreshold, rlim=0.0, ra=0, dec=0, torigin=0.0, noffset=0): #, ethreshold=100., evtclass=128):
+    """Look over spacecraft files and find time intervals which have more than N events.
+"""
+    metStart = tStart + torigin
+    metStop = tStop + torigin
+    periods_goodstat = [[tStart]]
+    nstat = 1
+    hdulistEVT = fits.open(pathFileEvt)
+    tbdataEVT = hdulistEVT[1].data
+    aTIME = tbdataEVT.field('TIME')
+    aRA = tbdataEVT.field('RA')
+    aDEC = tbdataEVT.field('DEC')
+    #aTIME.sort()
+    nEVT = len(aTIME)
+    time_prev = 0
+    coords_target = SkyCoord(ra, dec, unit="deg")
     print "  ", pathFileEvt, "(", nEVT, "events )"
-    for iEVT in range(nEVT):
+    for iEVT in range(1, nEVT):
         if aTIME[iEVT]<time_prev:
             print 'Odd order!!!'
             return 1
-        if aTIME[iEVT]>=metStart and aTIME[iEVT]<metStop: # and aEVTCLASS[iEVT]>=evtclass and aENERGY[iEVT]>ethreshold:
-            nstat+=1
-            time_fill = sqrt(aTIME[iEVT]*aTIME[iEVT+1])
-            if (nstat-noffset)/max(1., pow(sqrt(aTIME[iEVT]*aTIME[iEVT+1])-periods_goodstat[-1], 1./4.)) >= nthreshold and int(time_fill)>=int(periods_goodstat[-1]) and time_fill-periods_goodstat[-1]>=1:
-                periods_goodstat.append(time_fill)
+        if aTIME[iEVT]>=metStart and aTIME[iEVT]<metStop:
+            time_fill = sqrt((aTIME[iEVT-1]-torigin)*(aTIME[iEVT]-torigin))
+            if not rlim>0:
+                nstat+=1
+            else:
+                coords_event = SkyCoord(aRA[iEVT], aDEC[iEVT], unit="deg")
+                sep_ang = coords_target.separation(coords_event)
+                sep_deg = float(sep_ang.to_string(unit=u.deg, decimal=True))
+                if sep_deg<=rlim:
+                    nstat+=1
+                else:
+                    continue
+            if (nstat-noffset) >= nthreshold and int(time_fill)>=int(periods_goodstat[-1][0]): #and time_fill-periods_goodstat[-1][0]>=1:
+                periods_goodstat[-1].append(time_fill)
+                periods_goodstat.append([time_fill])
                 nstat = 0
         time_prev = aTIME[iEVT]
-#    if nstat<nthreshold:
-    periods_goodstat = periods_goodstat[:-1]
-    periods_goodstat.append(metStop)
+    if len(periods_goodstat)>1:
+        periods_goodstat = periods_goodstat[:-1]
+        periods_goodstat[-1][1] = tStop
+    else:
+        periods_goodstat[-1].append(tStop)
     return periods_goodstat
 
 
