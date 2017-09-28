404c8642aa350fa7e501cb52c3f317858b14ece5
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-09-28 07:07:22 -0700

    LightCurve.py: rough event count

--- STLikelihoodAnalysis/LightCurve.py
+++ STLikelihoodAnalysis/LightCurve.py
@@ -2,6 +2,10 @@
 """Module for making light curves of LAT data.
 The main class LightCurve is a chain of another module pLATLikelihoodConfig.py.
 The authour: Mitsunari Takahashi
+* Version: 1.7 (2017.09.27) 
+  Fit with weighting by yerr/ydata
+* Version: 1.6 (2017.09.26) 
+  More homogeneous time binning
 * Version: 1.5 (2017.09.22) 
   Energy vs. time plot.
 * Version: 1.4 (2017.09.22) 
@@ -23,6 +27,7 @@ path_upstairs = os.path.join(os.path.dirname(__file__), '../')
 sys.path.append(path_upstairs)
 import logging
 import pickle
+import subprocess
 import datetime
 import numpy as np
 import math
@@ -35,7 +40,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import STLikelihoodAnalysis.pLATLikelihoodConfig as pLATLikelihoodConfig
-from FindGoodstatPeriods import find_goodstat_periods
+from FindGoodstatPeriods import find_goodstat_periods, get_entries
 from FindCrossEarthlimb import find_cross_earthlimb
 from STLikelihoodAnalysis import get_module_logger
 import ReadLTFCatalogueInfo
@@ -46,7 +51,7 @@ mpl.rcParams['text.usetex'] = True
 mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
 
 ##### VERSION OF THIS MACRO #####
-VERSION = 1.5 # 2017.09.22
+VERSION = 1.7 # 2017.09.27
 
 
 ##### Logger #####
@@ -76,7 +81,7 @@ class LightCurve:
 
 
 class LightCurveGRB(LightCurve):
-    def __init__(self, name, tmin=0.0, tmax=10000.0, emin=100.0, emax=100000.0, evclass=128, evtype=3, ft2interval='30s', deg_roi=12., rad_margin=10., zmax=100., index_fixed=None, suffix='', grbcatalogue=pLATLikelihoodConfig.GRB_CATALOGUE_LTF, psForce=False, rlim_goodstat=1.0, ngoodstat=10., refit=True, force=False, outdir=None):
+    def __init__(self, name, tmin=0.0, tmax=10000.0, emin=100.0, emax=100000.0, evclass=128, evtype=3, ft2interval='30s', deg_roi=12., rad_margin=10., zmax=100., index_fixed=None, suffix='', grbcatalogue=pLATLikelihoodConfig.GRB_CATALOGUE_LTF, psForce=False, rlim_goodstat=2.0, ngoodstat=10, refit=True, force=False, outdir=None):
         # Target GRB
         self.grb = pLATLikelihoodConfig.GRBTarget(name, grbcatalogue)
 
@@ -95,13 +100,13 @@ class LightCurveGRB(LightCurve):
         self.analysis_whole.download()
         self.analysis_whole.set_directories()
         self.analysis_whole.filter(False)
-        self.analysis_whole.maketime(False)
+        self.analysis_whole.maketime(True)
 
         self.outdir = '{base}/{target}/{energy}/{roi}/{phase}'.format(base=self.analysis_whole.dir_base, target=self.analysis_whole.target.name, energy=self.analysis_whole.str_energy, roi=self.analysis_whole.str_roi, phase='lightcurve')
         self.outbasename = 'LightCurve_{target}_{spectype}_{index}{suffix}'.format(target=self.analysis_whole.target.name, spectype=self.analysis_whole.target.spectraltype, index=self.analysis_whole.str_index, suffix=self.analysis_whole.suffix)
 
         # Find good-statistics interval
-        validtimes = find_cross_earthlimb(self.analysis_whole.path_ft2, self.analysis_whole.target.ra, self.analysis_whole.target.dec, self.analysis_whole.tmin, self.analysis_whole.tmax, self.analysis_whole.zmax, self.analysis_whole.target.met0)
+        validtimes = find_cross_earthlimb(self.analysis_whole.path_ft2, self.analysis_whole.target.ra, self.analysis_whole.target.dec, self.analysis_whole.tmin, self.analysis_whole.tmax, self.analysis_whole.zmax, torigin=self.analysis_whole.target.met0)
         logger.info("""Good time intervals (zenith < {zen} deg)
 {vt}""".format(zen=self.config['zenith']['max'], vt=validtimes))
 
@@ -135,7 +140,14 @@ class LightCurveGRB(LightCurve):
             self.summary_results.append({})
             self.summary_results[-1]['time'] = {'min':self.analyses[-1].tmin, 'max':self.analyses[-1].tmax}
 
-            self.analyses[-1].setup(force={'download':self.config['force'], 'filter':self.config['force'], 'maketime':self.config['force'], 'livetime':self.config['force'], 'exposure':self.config['force'], 'model_3FGL_sources':True, 'diffuse_responses':self.config['force']})
+            nevt_rough = self.analyses[-1].setup(force={'download':self.config['force'], 'filter':self.config['force'], 'maketime':True, 'livetime':self.config['force'], 'exposure':self.config['force'], 'model_3FGL_sources':True, 'diffuse_responses':self.config['force']})
+
+            #nEVT_period = get_entries(self.analyses[-1].path_filtered_gti) #, pds[0], pds[1], self.analyses[-1].target.met0)
+            self.summary_results[-1]['Nobs'] = nevt_rough
+            if nevt_rough<1:
+                logger.info('No valid events. Skipping...')
+                self.summary_results[-1]['TS'] = 0
+                continue
 
             self.analyses[-1].fit(bredo=self.config['refit'])
             self.analyses[-1].summarize_fit_results()
@@ -157,8 +169,8 @@ class LightCurveGRB(LightCurve):
 
 
 
-def make_lightcurves(name, emin, emax, roi, ngoodstat, suffix, grbcatalogue, refit, force, outdir, index):
-    lc = LightCurveGRB(name=name, emin=emin, emax=emax, deg_roi=roi, ngoodstat=ngoodstat, suffix=suffix, grbcatalogue=grbcatalogue, refit=refit, force=force, outdir=None)
+def make_lightcurves(name, emin, emax, roi, ngoodstat, rgoodstat, suffix, grbcatalogue, refit, force, outdir, index):
+    lc = LightCurveGRB(name=name, emin=emin, emax=emax, deg_roi=roi, ngoodstat=ngoodstat, rlim_goodstat=rgoodstat, suffix=suffix, grbcatalogue=grbcatalogue, refit=refit, force=force, outdir=None)
     lc.setup()
     lc.count_energy()
     lc.run_analysis()
@@ -171,7 +183,7 @@ def dataframe(dct_summary, outdir=None, ts_threshold=4.0, index='free'):
     if isinstance(dct_summary, basestring) and dct_summary[-7:]=='.pickle':
         dct_summary = pickle_utilities.load(dct_summary)
     elif not isinstance(dct_summary, dict):
-        logger.critical('The input {0} was NOT a dictionary or path of pickle file!!!'.format(dct_summary[-7:]))
+        logger.critical('The input {0} was NOT a dictionary or path of pickle file!!!'.format(dct_summary))
         sys.exit(1)
 
     results = dct_summary['results']
@@ -221,6 +233,7 @@ def plot_lightcurves(dct_summary, outdir=None, ts_threshold=4.0, index='free', g
 
     # Config
     str_energies = '{emin:3.3f} - {emax:3.0f}'.format(emin=dct_summary['config']['energy']['min'], emax=dct_summary['config']['energy']['max'])
+    #dct_summary['fit'] = {'flux':{}}
 
     # Starting point of fitting
     tb_ltf = ReadLTFCatalogueInfo.open_table(1, grbcatalogue)
@@ -251,8 +264,9 @@ def plot_lightcurves(dct_summary, outdir=None, ts_threshold=4.0, index='free', g
         for period in dct_summary['results']:
             t0 = max(1, period['time']['min'])
             t1 = period['time']['max']
-            tref = (t0+t1)/2.0 #sqrt(t0*t1)
+            tref = 10**((np.log10(t0)+np.log10(t1))/2.0) #(t0+t1)/2.0 #sqrt(t0*t1)
             logger.info('----- {tmin:.1f} - {tmax:.1f} s -----'.format(tmin=t0, tmax=t1))            
+            logger.debug(tref)
             if curve.quantity == 'TS':
                 y = sqrt(max(0, period[curve.quantity]))
                 yerr = 0
@@ -275,11 +289,13 @@ def plot_lightcurves(dct_summary, outdir=None, ts_threshold=4.0, index='free', g
                         yerr_hi = yerr_hi*MEVtoERG
                         yerr_lo = yerr_lo*MEVtoERG
                     logger.info('{v:.2} + {eh:.2} - {el:.2}'.format(v=y, eh=yerr_hi, el=yerr_lo))
+
                 if curve.yerr_asym:
-                    curve.set_point(tref, y, {'lo':t1-tref, 'hi':tref-t0}, {'lo':yerr_lo, 'hi':yerr_hi})
+                    curve.set_point(tref, y, {'hi':t1-tref, 'lo':tref-t0}, {'lo':yerr_lo, 'hi':yerr_hi})
                 else:
-                    curve.set_point(tref, y, {'lo':t1-tref, 'hi':tref-t0}, yerr)
-            elif (curve.ul==True or curve.quantity in ('TS')) and period['TS']<ts_threshold:
+                    curve.set_point(tref, y, {'hi':t1-tref, 'lo':tref-t0}, yerr)
+
+            elif (curve.ul==True or curve.quantity in ('TS')) and period['TS']<ts_threshold and period['Nobs']>0:
                 if curve.quantity in ('flux', 'eflux', 'e2dnde'):
                     y = period['limits'][index][curve.quantity]['ul']
                     yerr = 0
@@ -305,15 +321,23 @@ def plot_lightcurves(dct_summary, outdir=None, ts_threshold=4.0, index='free', g
     logger.debug(ax[1].get_yticks())
     ax[1].set_yticks([y for y in ax[1].get_yticks() if y<0.5*ax[1].get_ylim()[1]])
     # Fit
-    params, butterfly = dct_curves['flux'].fit(t95, t95)
-    for iparam, param, in enumerate(params[0]):
-        logger.info("""Parameter {0}: {1} +/- {2}""".format(iparam, params[0][iparam], params[1][iparam]))
-    str_powerlaw_fitted = r"""$ \displaystyle F(t) = \rm{{ F_{{0}} }} \left( \frac{{ \it{{ t }} }}{{ \rm{{ T_{{ 95}} }} }} \right)^{{- \rm{{ \alpha }} }}$
+    fit_result = dct_curves['flux'].fit_lin(t95, t95)
+    dct_fit = {'fit':{'flux':{'afterglow':{}}}}
+    if isinstance(fit_result, tuple) and len(fit_result)==2:
+        params, butterfly = fit_result[0], fit_result[1]
+        for iparam, param, in enumerate(params[0]):
+            logger.info("""Parameter {0}: {1} +/- {2}""".format(iparam, params[0][iparam], params[1][iparam]))
+            str_powerlaw_fitted = r"""$ \displaystyle F(t) = \rm{{ F_{{0}} }} \left( \frac{{ \it{{ t }} }}{{ \rm{{ T_{{ 95}} }} }} \right)^{{- \rm{{ \alpha }} }}$
 $\rm{{ F_{{0}} = {f0:.2E} \pm {f0err:.1E} }}$
 $\rm{{ \alpha = {alpha:.2E} \pm {alphaerr:.2E} }}$""".format(f0=params[0][0], f0err=params[1][0], alpha=params[0][1], alphaerr=params[1][1])
-    ax[1].plot(butterfly[0], butterfly[1], c='g')
-    ax[1].fill_between(butterfly[0],butterfly[1]+butterfly[2],butterfly[1]-butterfly[2] ,alpha=0.2, facecolor='g')
-    ax[1].text(butterfly[0][0]*4, butterfly[1][0], str_powerlaw_fitted)
+        ax[1].plot(butterfly[0], butterfly[1], c='g')
+       #ax[1].fill_between(butterfly[0],butterfly[1]+butterfly[2],butterfly[1]-butterfly[2] ,alpha=0.2, facecolor='g')
+        ax[1].text(butterfly[0][0]*4, butterfly[1][0], str_powerlaw_fitted)
+        dct_fit['fit']['flux']['afterglow']['amplitude'] = {'value':params[0][0], 'error':params[1][0]}
+        dct_fit['fit']['flux']['afterglow']['index'] = {'value':params[0][1], 'error':params[1][1]}
+        dct_fit['fit']['flux']['afterglow']['t_scale'] = t95
+        dct_fit['fit']['flux']['afterglow']['tmin'] = t95
+        dct_fit['fit']['flux']['afterglow']['tmax'] = None
 
     ax[2].errorbar(dct_curves['eflux'].get_xdata(), dct_curves['eflux'].get_ydata(), xerr=dct_curves['eflux'].get_xerr(), yerr=dct_curves['eflux'].get_yerr(), fmt=dct_curves['eflux'].fmt)
     ax[2].errorbar(dct_curves['eflux_ul'].get_xdata(), dct_curves['eflux_ul'].get_ydata(), xerr=dct_curves['eflux_ul'].get_xerr(), fmt=dct_curves['eflux_ul'].fmt)
@@ -366,27 +390,52 @@ $\rm{{ \alpha = {alpha:.2E} \pm {alphaerr:.2E} }}$""".format(f0=params[0][0], f0
     outbasename = 'LightCurve_{target}_index{idx}{suffix}'.format(target=str(dct_summary['config']['name']), idx=index, suffix=str(dct_summary['config']['suffix']))
     for ff in ['png', 'pdf']:
         fig.savefig('{0}/{1}.{2}'.format(outdir, outbasename, ff))
+    pickle_utilities.dump('{0}/{1}_fit.pickle'.format(outdir, outbasename), dct_fit)
 
 
 @click.command()
-@click.argument('name', type=str)
+@click.option('--name', type=str)
 @click.option('--grbcatalogue', '-c', type=str, default=pLATLikelihoodConfig.GRB_CATALOGUE_LTF)
 @click.option('--emin', type=float, default=100.)
 @click.option('--emax', type=float, default=100000.)
 @click.option('--roi', type=float, default=12.)
 @click.option('--ngoodstat', type=int, default=10)
+@click.option('--rgoodstat', type=int, default=2)
 @click.option('--index', type=click.Choice(['free', 'best']), default='free')
 @click.option('--suffix', '-s', type=str, default='')
 @click.option('--force', '-f', is_flag=True)
 @click.option('--refit', '-r', is_flag=True)
 @click.option('--outdir', '-o', type=str, default='')
 @click.option('--plotonly', '-p', type=str, default=None, help='Path of result pickle file if you skip analyses.')
-def main(name, emin, emax, roi, ngoodstat, refit, force, suffix, grbcatalogue, outdir, plotonly, index):
-    if plotonly==None:
-        make_lightcurves(name, emin, emax, roi, ngoodstat, suffix, grbcatalogue, refit, force, outdir, index)
+@click.option('--bsub', '-b', is_flag=True)
+def main(name, emin, emax, roi, ngoodstat, rgoodstat, refit, force, suffix, grbcatalogue, outdir, plotonly, index, bsub):
+    if bsub==True:
+        tb_ltf = ReadLTFCatalogueInfo.open_table()#1, grbcatalogue)
+        tb_gbm = ReadLTFCatalogueInfo.select_gbm_exist(tb_ltf)
+        #tb_selected = ReadLTFCatalogueInfo.select_by_name(tb_gbm, namemin, namemax)
+        tb = tb_gbm
+        if len(tb)<1:
+            print 'No GRBs.'
+            return 1
+        for (irow , row) in enumerate(tb):
+            name = row['GRBNAME']
+            print '##### No.{0} GRB{1} #####'.format(irow, name)
+            if not os.path.exists(name):
+                os.mkdir(name)
+            acmd = ['bsub', '-o','{0}/GRB{0}_lightcurve{1}.log'.format(name, suffix if suffix=='' else '_'+suffix), '-J','lc{0}'.format(name), '-W','300', 'python', '/u/gl/mtakahas/work/PythonModuleMine/Fermi/STLikelihoodAnalysis/LightCurve.py', '--emin', str(emin), '--emax', str(emax), '-s', suffix, '--index', 'free', '--roi', str(roi), '--ngoodstat', str(ngoodstat), '--rgoodstat', str(rgoodstat), '--grbcatalogue', grbcatalogue, '--name', name]
+            if force==True:
+                acmd.append('--force')
+            if refit==True:
+                acmd.append('--refit')
+            print acmd
+            subprocess.call(acmd)
+        return 0
     else:
-        plot_lightcurves(plotonly, index=index, addphoton={'time': (422.7,), 'energy':(50.49609375*1000.,)})
-        dataframe(plotonly, index=index)
+        if plotonly==None:
+            make_lightcurves(name, emin, emax, roi, ngoodstat, rgoodstat, suffix, grbcatalogue, refit, force, outdir, index)
+        else:
+            plot_lightcurves(plotonly, index=index)#, addphoton={'time': (422.7,), 'energy':(50.49609375*1000.,)})
+            dataframe(plotonly, index=index)
 
 
 if __name__ == '__main__':
