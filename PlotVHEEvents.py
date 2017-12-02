8653cde3f41d34df72bb809421bacecaf7f9e648
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-12-01 23:46:24 -0800

    PlotVHEEvents.py: Additional CalOnly points

--- PlotVHEEvents.py
+++ PlotVHEEvents.py
@@ -46,9 +46,17 @@ MEVtoERG = 1.6021766208E-6
 GRB_CATALOGUE = '/nfs/farm/g/glast/u/mtakahas/FermiAnalysis/GRB/Regualr/catalogue/LAT2CATALOG-v1-LTF.fits'
 
 
+##### CalOnly photons #####
+addphoton={'090926A': {'time': (422.7,), 'energy':(50.49609375*1000.,)},
+           '150902A': {'time': (2064.52053469,), 'energy':(83.6322578125*1000.,)},
+           '160509A': {'time': (2035.85387415,5757.82151717), 'energy':(115.829539063*1000.,63.1624726562*1000.)}
+           }
+
+
 class SourceObject:
-    def __init__(self, name, met0, ra, dec, tmin=0, tmax=100000, emin=10000, emax=316228, deg_roi=1., zmax=100., suffix='', tprompt_start=0., redshift=0):
+    def __init__(self, name, met0, ra, dec, gcnname=None, tmin=0, tmax=100000, emin=10000, emax=316228, deg_roi=1., zmax=100., suffix='', tprompt_start=0., redshift=0):
         self.config = {'name':name, 
+                       'gcnname':gcnname if gcnname is not None else name,
                        'coord':{'ra':ra, 'dec':dec},
                        'time':{'min':tmin, 'max':tmax}, 
                        'met':{'o':met0, 'min':met0+tmin, 'max':met0+tmax}, 
@@ -69,10 +77,17 @@ class SourceObject:
         #    self.energies = self.energies * (1.+self.target['redshift'])
 
 
-    def plot(self, ax, nplot=0):
-        lst_markers = ['s', 'o', '*', 'D', 'x']
+    def plot(self, ax, nplot=0, addphoton=False):
+        lst_markers = ['s', 'o', 'D', 'x']
         nmarker = nplot / NMARKER_STYLE
-        ax.scatter(self.times, self.energies/1000., alpha=0.75, label=self.config['name'], marker=lst_markers[nmarker])
+        p = ax.scatter(self.times, self.energies/1000., alpha=0.75, label=self.config['gcnname'], marker=lst_markers[nmarker])
+        if addphoton==True and  self.config['gcnname'] in addphoton.keys():
+            print 'Additional photons:'
+            t_added = np.array(addphoton[self.config['gcnname']]['time'])
+            e_added = np.array(addphoton[self.config['gcnname']]['energy'])
+            for t,e in zip(t_added, e_added):
+                print '{e:.1f} MeV at {t:.1f}'.format(t=t, e=e)
+            ax.scatter(t_added, e_added/1000., marker=lst_markers[nmarker], label='CalOnly', facecolors='none', edgecolors=p.get_facecolors())
 
 
 @click.command()
@@ -87,10 +102,15 @@ class SourceObject:
 @click.option('--figform', type=str, default=('png',), multiple=True)
 @click.option('--longonly', '-l', is_flag=True)
 @click.option('--exceptions', '-e', type=str, default=(), multiple=True)
-def main(emin, emax, tmin, tmax, roi, redshift, suffix, pathout, figform, longonly, exceptions):
+@click.option('--tolerr', type=float, default=180)
+@click.option('--addphoton', '-a', is_flag=True)
+def main(emin, emax, tmin, tmax, roi, redshift, suffix, pathout, figform, longonly, exceptions, tolerr, addphoton):
     fig = plt.figure(figsize=(15,4.5))
     ax = fig.add_axes((0.07, 0.15, 0.9, 0.75))
-    ax.set_title('Events within {rad:2.1f} degrees from LAT-GRBs'.format(rad=roi))
+    str_title = 'Events within {rad:2.1f} deg from LAT-catalogued {lo}GRBs'.format(rad=roi, lo='long-' if longonly==True else '')
+    if tolerr<180:
+        str_title = str_title +  ' with localization error smaller than {te} deg'.format(te=tolerr)
+    ax.set_title(str_title)
     ax.set_xscale("log", nonposx='clip')
     ax.set_yscale("log", nonposy='clip')
     ax.set_xlabel(r'$Time - \rm{ min( T_{0}, T_{0}+T_{05}) [s]}$')
@@ -105,9 +125,10 @@ def main(emin, emax, tmin, tmax, roi, redshift, suffix, pathout, figform, longon
     lst_lat = ReadLATCatalogueInfo.select_gbm_exist(lst_lat)
     if longonly==True:
         lst_lat = ReadLATCatalogueInfo.select_long(lst_lat)
-    lst_lat = ReadLATCatalogueInfo.select_small_error(lst_lat, 0.3)
+    lst_lat = ReadLATCatalogueInfo.select_small_error(lst_lat, tolerr)
     ngrb_plotted = 0
     for (irow , tb_one) in enumerate(lst_lat):
+        #print tb_one
         name = tb_one['GRBNAME']
         print '##### No.{0} GRB{1} #####'.format(irow, name)
         if name in exceptions:
@@ -118,14 +139,14 @@ def main(emin, emax, tmin, tmax, roi, redshift, suffix, pathout, figform, longon
         t05 = pMETandMJD.ConvertMjdToMet(tb_one['GBM']['T90_START'])
         z = 0 #tb_one['REDSHIFT']
         if redshift==False or z>0:
-            src = SourceObject(name=name, met0=trigger_met, ra=tb_one['RA'], dec=tb_one['DEC'], tmin=tmin, tmax=tmax, emin=emin, emax=emax, deg_roi=roi, tprompt_start=t05, redshift=z if redshift==True else 0)
+            src = SourceObject(name=name, met0=trigger_met, ra=tb_one['RA'], dec=tb_one['DEC'], gcnname=tb_one['GCNNAME'], tmin=tmin, tmax=tmax, emin=emin, emax=emax, deg_roi=roi, tprompt_start=t05, redshift=z if redshift==True else 0)
             path_dir_data = '/u/gl/mtakahas/work/FermiAnalysis/GRB/Regualr/HighestFluenceGRBs/LatAlone/{na}'.format(na=name)
             if not os.path.exists(path_dir_data):
                 os.mkdir(path_dir_data)
             #DownloadFermiData.download_fermi_data_grb(name, lst_ft=[1], path_outdir=path_dir_data)
             src.get_events()
             if len(src.times)>0:
-                src.plot(ax, ngrb_plotted)
+                src.plot(ax, ngrb_plotted, addphoton)
                 ngrb_plotted+=1
     ax.set_xlim([max(0.1,tmin), tmax])
     ax.set_ylim([emin/1000., emax/1000.])
@@ -133,7 +154,7 @@ def main(emin, emax, tmin, tmax, roi, redshift, suffix, pathout, figform, longon
     ax.grid(axis='y', which='minor',color='black',linestyle='-.', alpha=0.25)
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
     ax.yaxis.set_minor_formatter(FormatStrFormatter('%.0f'))
-    ax.legend(loc=0, fancybox=True, framealpha=0.5, fontsize=9, ncol=ngrb_plotted/10+1)
+    ax.legend(loc=0, fancybox=True, framealpha=0.5, fontsize=9, ncol=ngrb_plotted/7+1)
     for ff in figform:
         fig.savefig('{0}{1}{2}.{3}'.format(pathout, suffix if suffix=='' else '_'+suffix, '_redshift' if redshift==True else '', ff))
 
