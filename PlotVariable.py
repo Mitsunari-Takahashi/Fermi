6e816f3e2f9184baad7653e506f7caef0bd01a31
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-12-15 07:14:19 -0800

    PlotVariable.py: still normalization does not work well

--- PlotVariable.py
+++ PlotVariable.py
@@ -48,7 +48,7 @@ def main(name, definition, ag, bkg, nbine, nbincth, logy):
     trBKG = fileBKG.Get("MeritTuple")
     print trAG.GetName(), "is found."
 
-    strCutBase = "( Cal1RawEnergySum>=20000 && FswGamState==0 )"
+    strCutBase = "( Cal1RawEnergySum>=20000 && FswGamState==0 )" # && Cal1MomZCrossSide840>=0.0 && Cal1MomZDir>=0.2 && Cal1MomNumIterations>0
     strCutCalOnly = "( TkrNumTracks==0 || (log10(max(CalTrackAngle,1E-4)) > (0.529795)*(EvtJointLogEnergy < 3.000000)  + ((1.0)*((0.529795)*(1.0)+(-1.379791)*(pow((EvtJointLogEnergy-3.000000)/0.916667,1))+(0.583401)*(pow((EvtJointLogEnergy-3.000000)/0.916667,2))+(-0.075555)*(pow((EvtJointLogEnergy-3.000000)/0.916667,3))))*(EvtJointLogEnergy  >= 3.000000 && EvtJointLogEnergy <= 5.750000) + (-0.398962)*(EvtJointLogEnergy >  5.750000))  )"
     strCutHad = "( (McSourceId >= 1000 && McSourceId < 2000) || (McSourceId>=4000 && McSourceId<7000) )"
     strCutLep = "( McSourceId >= 2000 && McSourceId < 4000 )"
@@ -64,14 +64,20 @@ def main(name, definition, ag, bkg, nbine, nbincth, logy):
 
     h3Gam = ROOT.TH3F("h3Gam_"+name, "Gammas", NBIN_ENE, LOWEDGE_ENE, UPEDGE_ENE, NBIN_CTH, LOWEDGE_CTH, UPEDGE_CTH, NBIN_VAR, minCommon, maxCommon)
     h3Gam.SetLineColor(ROOT.kBlue)
+    h3Gam.SetLineWidth(2)
+    h3Gam.SetFillStyle(3004)
     h3Gam.SetFillColor(ROOT.kBlue)
     h3Gam.Write()
     h3Had = ROOT.TH3F("h3Had_"+name, "Hadrons", NBIN_ENE, LOWEDGE_ENE, UPEDGE_ENE, NBIN_CTH, LOWEDGE_CTH, UPEDGE_CTH, NBIN_VAR, minCommon, maxCommon)
     h3Had.SetLineColor(ROOT.kRed)
+    h3Had.SetLineWidth(2)
+    h3Had.SetFillStyle(3005)
     h3Had.SetFillColor(ROOT.kRed)
     h3Had.Write()
     h3Lep = ROOT.TH3F("h3Lep_"+name, "Leptons", NBIN_ENE, LOWEDGE_ENE, UPEDGE_ENE, NBIN_CTH, LOWEDGE_CTH, UPEDGE_CTH, NBIN_VAR, minCommon, maxCommon)
     h3Lep.SetLineColor(ROOT.kMagenta)
+    h3Lep.SetLineWidth(2)
+    h3Lep.SetFillStyle(3006)
     h3Lep.SetFillColor(ROOT.kMagenta)
     h3Lep.Write()
 
@@ -97,19 +103,32 @@ def main(name, definition, ag, bkg, nbine, nbincth, logy):
         aHtgLep.append([])
         for iC in range(NBIN_CTH):
             print "  Inclination bin No.", iC+1, "-McZDir:", h3Gam.GetYaxis().GetBinLowEdge(iC+1), " - ", h3Gam.GetYaxis().GetBinUpEdge(iC+1)
-            aHsPlot[-1].append(ROOT.THStack("hs{0}_{1}_{2}".format(name, iE+1, iC+1), "{0}<=McLogEnergy<{1} and {2}<=-McZDir<{3}".format(h3Gam.GetXaxis().GetBinLowEdge(iE+1), h3Gam.GetXaxis().GetBinUpEdge(iE+1), h3Gam.GetYaxis().GetBinLowEdge(iC+1), h3Gam.GetYaxis().GetBinUpEdge(iC+1))))
+            aHsPlot[-1].append(ROOT.THStack("hs{0}_{1}_{2}".format(name, iE+1, iC+1), "{0:1.2f}<=McLogEnergy<{1:1.2f} and {2:1.2f}<=-McZDir<{3:1.2f}".format(h3Gam.GetXaxis().GetBinLowEdge(iE+1), h3Gam.GetXaxis().GetBinUpEdge(iE+1), h3Gam.GetYaxis().GetBinLowEdge(iC+1), h3Gam.GetYaxis().GetBinUpEdge(iC+1))))
             aHtgGam[-1].append(h3Gam.ProjectionZ("{0}_projZ_{1}_{2}".format(h3Gam.GetName(), iE+1, iC+1), iE+1, iE+1, iC+1, iC+1))
+            print '    Integral of {0}: {1}'.format(aHtgGam[-1][-1].GetName(), aHtgGam[-1][-1].Integral())
             if aHtgGam[-1][-1].Integral()>0:
-                aHtgGam[-1][-1].Scale(1./aHtgGam[-1][-1].Integral())
+                aHtgGam[-1][-1].Scale(1./aHtgGam[-1][-1].Integral(1, aHtgGam[-1][-1].GetNbinsX()))
                 aHsPlot[-1][-1].Add(aHtgGam[-1][-1])
+                print '    Integral of {0}: {1}'.format(aHtgGam[-1][-1].GetName(), aHtgGam[-1][-1].Integral())
+            else:
+                print '    Integral of {0} is zero!'.format(aHtgGam[-1][-1].GetName())
             aHtgHad[-1].append(h3Had.ProjectionZ("{0}_projZ_{1}_{2}".format(h3Had.GetName(), iE+1, iC+1), iE+1, iE+1, iC+1, iC+1))
+            print '    Integral of {0}: {1}'.format(aHtgHad[-1][-1].GetName(), aHtgHad[-1][-1].Integral())
             if aHtgHad[-1][-1].Integral()>0:
-                aHtgHad[-1][-1].Scale(1./aHtgHad[-1][-1].Integral())
+                aHtgHad[-1][-1].Scale(1./aHtgHad[-1][-1].Integral(1, aHtgGam[-1][-1].GetNbinsX()))
                 aHsPlot[-1][-1].Add(aHtgHad[-1][-1])
+                print '    Integral of {0}: {1}'.format(aHtgHad[-1][-1].GetName(), aHtgHad[-1][-1].Integral())
+            else:
+                print '    Integral of {0} is zero!'.format(aHtgHad[-1][-1].GetName())
             aHtgLep[-1].append(h3Lep.ProjectionZ("{0}_projZ_{1}_{2}".format(h3Lep.GetName(), iE+1, iC+1), iE+1, iE+1, iC+1, iC+1))
+            print '    Integral of {0}: {1}'.format(aHtgLep[-1][-1].GetName(), aHtgLep[-1][-1].Integral())
             if aHtgLep[-1][-1].Integral()>0:
-                aHtgLep[-1][-1].Scale(1./aHtgLep[-1][-1].Integral())
+                aHtgLep[-1][-1].Scale(1./aHtgLep[-1][-1].Integral(1, aHtgGam[-1][-1].GetNbinsX()))
                 aHsPlot[-1][-1].Add(aHtgLep[-1][-1])
+                print '    Integral of {0}: {1}'.format(aHtgLep[-1][-1].GetName(), aHtgLep[-1][-1].Integral())
+            else:
+                print 'Integral of {0} is zero!'.format(aHtgLep[-1][-1].GetName())
+
             # Calculate Minimum Entropy
             # entropy_had = 1000
             # entropy_lep = 1000
@@ -132,9 +151,9 @@ def main(name, definition, ag, bkg, nbine, nbincth, logy):
             #             entropy_lep = entropy_lep_temp
             #             var_min_ent_lep = aHtgLep[-1][-1].GetZaxis().GetBinLowEdge(ibvar)
 
-            ipad = ipad + 1
+            ipad = iE+7*iC+1#ipad + 1
             cPlot.cd(ipad)
-            aHsPlot[-1][-1].Draw()
+            aHsPlot[-1][-1].Draw("nostack")
             if logy==True:
                 ROOT.gPad.SetLogy()
             # latex = ROOT.TLatex()
