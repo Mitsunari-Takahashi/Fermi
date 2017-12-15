1d9c2ab93da3c86ad4233825415eefec68dc867d
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2017-12-15 07:19:00 -0800

    STLikelihoodGRBAnalysis.py: Eref, sptime

--- STLikelihoodAnalysis/STLikelihoodGRBAnalysis.py
+++ STLikelihoodAnalysis/STLikelihoodGRBAnalysis.py
@@ -17,23 +17,23 @@ import pickle_utilities
 logger = get_module_logger(__name__)
 
 
-def likelihood_grb_analysis(name, mode, emin, emax, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, outdir, binned, masifps, scalefactor=1., gti_external=None):
+def likelihood_grb_analysis(name, mode, emin, emax, eref, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, sptmin, sptmax, outdir, binned, masifps, scalefactor=1., gti_external=None):
     if spectraltype=='PowerLaw':
-        spectralpars = {'Prefactor':1e-10, 'Index':-2.0, 'Scale':1000.}
+        spectralpars = {'Prefactor':1e-10, 'Index':-2.0, 'Scale':eref}
     elif spectraltype=='PowerLaw2':
         spectralpars = {'Integral':1e-5, 'Index':-2.0, 'LowerLimit':emin, 'UpperLimit':emax}
     elif spectraltype=='ScaleFactor::PowerLaw2':
         spectralpars = {'Integral':1e-5, 'Index':-2.0, 'LowerLimit':emin, 'UpperLimit':emax, 'ScaleFactor':scalefactor}
     elif spectraltype=='ExpCutoff':
-        spectralpars = {'Prefactor':1e-10, 'Index':-2.0, 'Scale':1000., 'Ebreak':10.0, 'P1':10000., 'P2':0, 'P3':0}
+        spectralpars = {'Prefactor':1e-10, 'Index':-2.0, 'Scale':eref, 'Ebreak':10.0, 'P1':10000., 'P2':0, 'P3':0}
     elif spectraltype=='BrokenPowerLaw':
-        spectralpars = {'Prefactor':1e-12, 'Index1':-2.0, 'Index2':-2.0, 'BreakValue':10000.}
+        spectralpars = {'Prefactor':1e-12, 'Index1':-2.0, 'Index2':-2.0, 'BreakValue':eref}
     else:
         logger.critical("""{0} is NOT available!!! Use PowerLaw or ExpCutoff.""".format(spectraltype))
         sys.exit(1)
 
     grb = pLATLikelihoodConfig.GRBTarget(name, grbcatalogue, spectraltype=spectraltype, spectralpars=spectralpars)
-    ana = pLATLikelihoodConfig.GRBConfig(grb, mode, emin=emin, emax=emax, deg_roi=roi, binned=binned, psForce=masifps, ft2interval='1s', gti_external=gti_external)
+    ana = pLATLikelihoodConfig.GRBConfig(grb, mode, emin=emin, emax=emax, deg_roi=roi, tmin_special=sptmin, tmax_special=sptmax, binned=binned, psForce=masifps, ft2interval='1s', gti_external=gti_external)
     if ana.tmin == ana.tmax:
         logger.warning('Time range of GRB config is NOT valid!')
         sys.exit(1)
@@ -58,6 +58,13 @@ def likelihood_grb_analysis(name, mode, emin, emax, roi, spectraltype, refit, fo
         if spectraltype in ('PowerLaw', 'PowerLaw2', 'ScaleFactor::PowerLaw2'):
             ana.eval_limits_powerlaw(str_index_fixed=['best'])
     logger.info(ana.dct_summary_results)
+
+    ##### Likelihood ratio ordering #####
+    if nrough<1:
+        ana.order_likeratio(nseries=1)
+    elif nrough<5:
+        ana.order_likeratio(nseries=0)
+    
     pickle_utilities.dump('{0}/Summary{1}.pickle'.format(ana.dir_work, '' if suffix=='' else '_'+suffix), ana.dct_summary_results)
     return ana.dct_summary_results
 
@@ -70,6 +77,7 @@ def likelihood_grb_analysis(name, mode, emin, emax, roi, spectraltype, refit, fo
 @click.option('--mode', '-m', type=click.Choice(['unified', 'prompt', 'primary', 'intermittent', 'afterglow', 'earlyAG', 'lateAG', 'farAG', 'T95to01ks', 'T95to03ks', '01ksto10ks', '01ksto100ks', 'T95to03ks', '03ksto10ks', '03ksto100ks', 'lightcurve', 'special']))
 @click.option('--emin', type=float, default=100.)
 @click.option('--emax', type=float, default=100000.)
+@click.option('--eref', type=float, default=1000.)
 @click.option('--roi', type=float, default=12.)
 @click.option('--spectraltype', type=click.Choice(['PowerLaw', 'PowerLaw2', 'ScaleFactor::PowerLaw2', 'ExpCutoff', 'BrokenPowerLaw']), default='PowerLaw')
 @click.option('--suffix', '-s', type=str, default='')
@@ -77,11 +85,13 @@ def likelihood_grb_analysis(name, mode, emin, emax, roi, spectraltype, refit, fo
 @click.option('--modelonly', is_flag=True)
 @click.option('--masifps', is_flag=True)
 @click.option('--refit', '-r', is_flag=True)
+@click.option('--sptmin', type=float, default=-1, help='Only for mode special')
+@click.option('--sptmax', type=float, default=-1, help='Only for mode special')
 @click.option('--binned', '-b', is_flag=True)
 @click.option('--outdir', '-o', type=str, default='/u/gl/mtakahas/work/FermiAnalysis/GRB/Regualr/HighestFluenceGRBs/LatAlone')
 @click.option('--quanta', '-q', type=str, default='/u/gl/mtakahas/work/FermiAnalysis/GRB/Regualr/HighestFluenceGRBs/LatAlone/QuantiledGRBs_longonly3_GTI.pickle')
 @click.option('--bsub', is_flag=True)
-def main(namemin, namemax, mode, emin, emax, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, outdir, binned, masifps, quanta, bsub):
+def main(namemin, namemax, mode, emin, emax, eref, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, sptmin, sptmax, outdir, binned, masifps, quanta, bsub):
 
     tb_lat = ReadLATCatalogueInfo.open_table()
     tb_gbm = ReadGBMCatalogueInfo.open_table()
@@ -98,11 +108,12 @@ def main(namemin, namemax, mode, emin, emax, roi, spectraltype, refit, force, su
         scalefactor = 1
         if spectraltype[:13]=='ScaleFactor::':
             scalefactors = dct_quantile['fluence_scaled_gbm']['scaled'][namemin]
-        for idx, pha in itertools.product(range(len(lc_indices)), range(len(phases))):
-            #print lc_indices[idx], phases[pha]
-            if lc_indices[idx]==-1 and phases[pha]==mode:
-                logger.info('ScaleFactor = {0}'.format(scalefactors[idx][pha]))
-                likelihood_grb_analysis(namemin, mode, emin, emax, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, outdir, binned, masifps, scalefactor=scalefactors[idx][pha])
+        else:
+            scalefactors = 1
+        # for idx, pha in itertools.product(range(len(lc_indices)), range(len(phases))):
+        #     if lc_indices[idx]==-1 and phases[pha]==mode:
+        #         logger.info('ScaleFactor = {0}'.format(scalefactors[idx][pha]))
+        likelihood_grb_analysis(namemin, mode, emin, emax, eref, roi, spectraltype, refit, force, suffix, grbcatalogue, modelonly, sptmin, sptmax, outdir, binned, masifps) #, scalefactor=scalefactors[idx][pha])
     else:
         tb = ReadLATCatalogueInfo.select_by_name(tb_lat, namemin, namemax, tb_gbm)
         tb = ReadLATCatalogueInfo.select_gbm_exist(tb)
@@ -114,7 +125,7 @@ def main(namemin, namemax, mode, emin, emax, roi, spectraltype, refit, force, su
             print '##### No.{0} GRB{1} #####'.format(irow, name)
             if not os.path.exists(name):
                 os.mkdir(name)
-            acmd = ['bsub', '-o','{0}/{1}/GRB{1}_{2}{3}.log'.format(outdir, name, mode, suffix if suffix=='' else '_'+suffix), '-J','{0}{1}'.format(name[:6], mode[:2]), '-W','300', 'python', '/u/gl/mtakahas/work/PythonModuleMine/Fermi/STLikelihoodAnalysis/STLikelihoodGRBAnalysis.py', '-m', mode, '--emin', str(emin), '--emax', str(emax), '-s', suffix, '--roi', str(roi), '--spectraltype', spectraltype, '--outdir', '{0}/{1}'.format(outdir, name), '--namemin', name, '--quanta', quanta]
+            acmd = ['bsub', '-o','{0}/{1}/GRB{1}_{2}{3}.log'.format(outdir, name, mode, suffix if suffix=='' else '_'+suffix), '-J','{0}{1}'.format(name[:6], mode[:2]), '-W','300', 'python', '/u/gl/mtakahas/work/PythonModuleMine/Fermi/STLikelihoodAnalysis/STLikelihoodGRBAnalysis.py', '-m', mode, '--emin', str(emin), '--emax', str(emax), '--eref', str(eref), '-s', suffix, '--roi', str(roi), '--spectraltype', spectraltype, '--sptmin', str(sptmin), '--sptmax', str(sptmax), '--outdir', '{0}/{1}'.format(outdir, name), '--namemin', name, '--quanta', quanta]
             if force==True:
                 acmd.append('--force')
             if refit==True:
