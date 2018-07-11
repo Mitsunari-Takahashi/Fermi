5661e8c7583c9cdb50ea3d8a8694ca4b10042fc6
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2018-07-11 06:37:15 -0700

    pEvalEventEnergyError.py: Use CalOnly energy

--- pEvalEventEnergyError.py
+++ pEvalEventEnergyError.py
@@ -9,7 +9,7 @@ import click
 from ctypes import *
 from array import array
 import ROOT
-from ROOT import gROOT, gDirectory, gPad, gSystem, gStyle, kTRUE, kFALSE, TTree
+from ROOT import gROOT, gDirectory, gPad, gSystem, gStyle, kTRUE, kFALSE, TTree, TH1, TH2, THStack
 from array import array
 from pColor import *
 ROOT.gROOT.SetBatch() 
@@ -18,34 +18,41 @@ ROOT.gROOT.SetBatch()
 @click.argument('e', type=float)
 @click.argument('bep', type=float)
 @click.option('--fileCut', '-c', default='/nfs/farm/g/glast/u/mtakahas/v20r09p09_G1haB1/S18/S18V200909_020RAWE20ZDIR020ZCS000wwoTRKwoMCZDIR00woRWcatTwo_15/hBDT_CutValues_S18V200909_020RAWE20ZDIR020ZCS000wwoTRKwoMCZDIR00woRWcatTwo_15_catZDIR060_BDTG500D06_catZDIR060.root')
-@click.option('--fileBDT', '-b', default='/u/gl/mtakahas/work/data/MC/AG200909_62_2016Jun_S18V200909_020RAWE20ZDIR020ZCS000wwoTRKwoMCZDIR00woRWcatTwo_15_catZDIR060_BDTG500D06_catZDIR060.root')
-@click.option('--fileOut', '-o', default='hEnergyUncertainty.root')
+@click.option('--filebdt', '-b', default='/u/gl/mtakahas/work/data/MC/AG200909_62_2016Jun_S18V200909_020RAWE20ZDIR020ZCS000wwoTRKwoMCZDIR00woRWcatTwo_15_catZDIR060_BDTG500D06_catZDIR060.root')
+@click.option('--outdir', '-d', default='.')
+@click.option('--outname', '-o', default='hEnergyUncertainty')
 @click.option('--htgCut', '-t', default='htgCutBDT0')
+@click.option('--etol', type=float, default=0.1, help='Tolerance of event energy')
+@click.option('--beptol', type=float, default=0.05, help='Tolerance of event BEP')
 @click.option('--bdt', '-v', default='S18V200909_020RAWE20ZDIR020ZCS000wwoTRKwoMCZDIR00woRWcatTwo_15_catZDIR060_BDTG500D06_catZDIR060')
-def main(e, bep, filecut, filebdt, fileout, htgcut, bdt):
+def main(e, bep, filecut, filebdt, outdir,outname, htgcut, etol, beptol, bdt):
+    print 'BDT cut:', bep*beptol
     eLin = pow(10, e-3)
     fileAG = ROOT.TFile('/nfs/farm/g/glast/u/mtakahas/data/MC/AG200909_62_2016Jun.root')
     trAG = fileAG.Get('MeritTuple')
     nEvt = trAG.GetEntries()
-    fileBDT = ROOT.TFile(filebdt)
+    fileBDT = ROOT.TFile(filebdt, "READ")
     trBDT = fileBDT.Get('MeritTuple')
     trAG.AddFriend(trBDT, 'trTempBDT')
     bdtR = c_float() 
     print bdt
     trAG.SetBranchAddress('{0}'.format(bdt), bdtR)
-    aPowIndex = [0, 1, 2, 3]
+    aPowIndex = [1, 2, 3]
+    DICT_COLOR = {0: ROOT.kGray, 1: ROOT.kBlue, 2: ROOT.kRed, 3: ROOT.kMagenta}
     dictHtg2D = {}
     dictHtgProjX = {}
-    fileOutEU = ROOT.TFile(fileout, 'RECREATE')
+    fileOutEU = ROOT.TFile('{di}/{na}_ETOL{et:0>3.0f}_BEPTOL{bt:0>3.0f}.root'.format(di=outdir, na=outname, et=etol*100, bt=beptol*100), "RECREATE")
     for pi in aPowIndex:
-        dictHtg2D[pi] = ROOT.TH2D('htg2D_{0}'.format(int(pi*10)), 'Power-law index -{0};EvtJointLogEnergy-{1};McLogEnergy-{1}'.format(pi, e), 100, -1.0, 1.0, 100, -1.0, 1.0)
+        dictHtg2D[pi] = ROOT.TH2D('htg2D_{0}'.format(int(pi*10)), 'Power-law index -{0};log10(WP8CalOnlyEnergy)-{1};McLogEnergy-{1}'.format(pi, e), 200, -1.0, 1.0, 1000, -1.0, 1.0)
     fileCutBDT = ROOT.TFile(filecut)
     htgCut = fileCutBDT.Get(htgcut)
     for iEvt in range(nEvt):
         trAG.GetEntry(iEvt)
-        if trAG.Cal1MomZCrossSide840>=0.0 and trAG.Cal1MomNumIterations>0 and trAG.Cal1TransRms>=10 and trAG.Cal1TransRms<70 and trAG.Acd2Cal1VetoSigmaHit>0 and trAG.Cal1MomZDir>=0.2 and -log10(1.0-(1.0+bdtR.value)/2.)>=htgCut.GetBinContent(htgCut.GetXaxis().FindBin(trAG.EvtJointLogEnergy),htgCut.GetYaxis().FindBin(trAG.Cal1MomZDir)) and trAG.Cal1RawEnergySum>=20000 and (trAG.TkrNumTracks==0 or (math.log10(max(trAG.CalTrackAngle,1E-4)) > (0.529795)*(trAG.EvtJointLogEnergy < 3.000000)  + ((1.0)*((0.529795)*(1.0)+(-1.379791)*(math.pow((trAG.EvtJointLogEnergy-3.000000)/0.916667,1))+(0.583401)*(math.pow((trAG.EvtJointLogEnergy-3.000000)/0.916667,2))+(-0.075555)*(math.pow((trAG.EvtJointLogEnergy-3.000000)/0.916667,3))))*(trAG.EvtJointLogEnergy >= 3.000000 and trAG.EvtJointLogEnergy <= 5.750000) + (-0.398962)*(trAG.EvtJointLogEnergy >  5.750000))) and trAG.FswGamState==0 and abs((trAG.WP8CalOnlyBEPCaseE_myBDT+1.)/2.-bep)<0.05:# and abs((trAG.WP8CalOnlyBEPCaseE_myBDT+1.)/2.-bep)<0.01: #and (trAG.GltGemSummary&0x20)==0):
-            for pi in aPowIndex:
-                dictHtg2D[pi].Fill(trAG.McLogEnergy-trAG.EvtJointLogEnergy, trAG.EvtJointLogEnergy-e, pow(10, (-pi+1)*trAG.McEnergy/pow(10, e)))
+        if trAG.WP8CalOnlyEnergy>0:
+            logcale = log10(trAG.WP8CalOnlyEnergy)
+            if trAG.Cal1MomZCrossSide840>=0.0 and trAG.Cal1MomNumIterations>0 and trAG.Cal1TransRms>=10 and trAG.Cal1TransRms<70 and trAG.Acd2Cal1VetoSigmaHit>0 and trAG.Cal1MomZDir>=0.2 and -log10(1.0-(1.0+bdtR.value)/2.)>=htgCut.GetBinContent(htgCut.GetXaxis().FindBin(logcale),htgCut.GetYaxis().FindBin(trAG.Cal1MomZDir)) and trAG.Cal1RawEnergySum>=20000 and (trAG.TkrNumTracks==0 or (math.log10(max(trAG.CalTrackAngle,1E-4)) > (0.529795)*(trAG.EvtJointLogEnergy < 3.000000)  + ((1.0)*((0.529795)*(1.0)+(-1.379791)*(math.pow((trAG.EvtJointLogEnergy-3.000000)/0.916667,1))+(0.583401)*(math.pow((trAG.EvtJointLogEnergy-3.000000)/0.916667,2))+(-0.075555)*(math.pow((trAG.EvtJointLogEnergy-3.000000)/0.916667,3))))*(trAG.EvtJointLogEnergy >= 3.000000 and trAG.EvtJointLogEnergy <= 5.750000) + (-0.398962)*(trAG.EvtJointLogEnergy >  5.750000))) and trAG.FswGamState==0 and abs((trAG.WP8CalOnlyBEPCaseE_myBDT+1.)/2.-bep)<bep*beptol:# and abs((trAG.WP8CalOnlyBEPCaseE_myBDT+1.)/2.-bep)<0.01: #and (trAG.GltGemSummary&0x20)==0):
+                for pi in aPowIndex:
+                    dictHtg2D[pi].Fill(trAG.McLogEnergy-logcale, logcale-e, pow(10, (-pi+1)*trAG.McEnergy/pow(10, e)))
                 #dictHtg2D[pi].Fill(trAG.McLogEnergy-e, trAG.EvtJointLogEnergy-e, pow(10, (-pi+1)*trAG.McEnergy/pow(10, e)))
     aPos = [0.68, 0.95]
     aPosLeft = array('d', [1.-aPos[0], 1-aPos[1]])
@@ -53,13 +60,21 @@ def main(e, bep, filecut, filebdt, fileout, htgcut, bdt):
     aQuantLeft = array('d', [0., 0.])
     aQuantRight = array('d', [0., 0.])
     fileOutEU.cd()
+    hs = THStack('hs', r'MC gamma events;$\mathrm{log_{10}}E_{true} - \mathrm{log_{10}}E_{rec}$;PDF')
     for pi in aPowIndex:
         print '=====  Index:', pi, '  ====='
         dictHtg2D[pi].Write()
-        nBinCutLowY = dictHtg2D[pi].GetYaxis().FindBin(-0.05) #(-0.005)
-        nBinCutUpY = dictHtg2D[pi].GetYaxis().FindBin(0.05) #(0.005)
+        nBinCutLowY = dictHtg2D[pi].GetYaxis().FindBin(-etol) #(-0.005)
+        nBinCutUpY = dictHtg2D[pi].GetYaxis().FindBin(+etol) #(0.005)
         print 'Bin:', nBinCutLowY, '-', nBinCutUpY
+        print 'Energies:', 10**(dictHtg2D[pi].GetYaxis().GetBinLowEdge(nBinCutLowY)+e), '-', 10**(dictHtg2D[pi].GetYaxis().GetBinUpEdge(nBinCutUpY)+e), 'MeV'
         dictHtgProjX[pi] = dictHtg2D[pi].ProjectionX('htgProjX_{0}'.format(int(pi*10)), nBinCutLowY, nBinCutUpY)
+        dictHtgProjX[pi].SetLineColor(DICT_COLOR[pi])
+        dictHtgProjX[pi].SetLineWidth(2)
+        dictHtgProjX[pi].SetMarkerColor(DICT_COLOR[pi])
+        dictHtgProjX[pi].SetFillStyle(0)
+        dictHtgProjX[pi].Scale(1./dictHtgProjX[pi].Integral())
+        hs.Add(dictHtgProjX[pi])
         dictHtgProjX[pi].Write()
         hLeft = dictHtgProjX[pi].Clone('hTempLeft')
         hRight = dictHtgProjX[pi].Clone('hTempRight')
@@ -75,7 +90,8 @@ def main(e, bep, filecut, filebdt, fileout, htgcut, bdt):
         for (iPos, nPos) in enumerate(aPos):
             print '{0}% range of {1:.1f} GeV: {2:.1f} - {3:.1f} GeV, Error: {1:.1f} -{4:.1f}+{5:.1f} GeV'.format(int(nPos*100), eLin, pow(10, e+aQuantLeft[iPos]-3), pow(10, e+aQuantRight[iPos]-3), (eLin-pow(10, e+aQuantLeft[iPos]-3)), (pow(10, e+aQuantRight[iPos]-3)-eLin))
             #print '{0}% Error: {1}-{2}+{3} GeV'.format(int(nPos*100), eLin, eLin-pow(10, e+aQuantLeft[iPos]-3), pow(10, e+aQuantRight[iPos]-3)-eLin)
-        
+    hs.Write()
+
 
 if __name__ == '__main__':
     main()
