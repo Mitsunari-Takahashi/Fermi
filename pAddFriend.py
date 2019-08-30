36c3d497cd3ac970b37c40bdc53ec3ae26ea63c9
Mitsunari Takahashi <takhsm@icrr.u-tokyo.ac.jp>
2019-08-30 03:08:36 -0700

    pAddFriend.py: Implement Updown variable

--- pAddFriend.py
+++ pAddFriend.py
@@ -2,27 +2,17 @@
 
 import ROOT
 import sys
-#from optparse import OptionParser
-
-# if __name__ == '__main__':
-#     p = OptionParser(version="ver:%s" % __version__)
-#     p.add_option('--BEP', action='store_true', dest="bep", default=False,
-#                  help="Add BEP variable as a friend.")
-#     (opts, args) = p.parse_args()
 ROOT.gROOT.SetBatch()
-par = sys.argv #args # 
+par = sys.argv 
 print par
 aPathFile = par[1:]
 
-aSuffixFriend=["newVariables", "WP8CalOnlyBEPCaseE_myBDT", "CalOnlyVar"]
-aNameFriend=["newVariables", "WP8CalOnlyBEP_E_BDT", "CalOnlyVar"]
+aSuffixFriend=["U01_V200909_62IRFBK_020RAWE20woRW_09", "newVariables", "WP8CalOnlyBEPCaseE_myBDT", "CalOnlyVar"]
+aNameFriend=["UpDown", "newVariables", "WP8CalOnlyBEP_E_BDT", "CalOnlyVar"]
 if len(aSuffixFriend)!=len(aNameFriend):
     print "Numbers of the name and the suffix of the friend files are different!!"
     sys.exit(1)
 nFriend = len(aSuffixFriend)
-# if options.bep==True:
-#     nFriend = nFriend+1
-#     aSuffixFriend.append("")
 for pathFile in aPathFile:
     if pathFile[0] is not "/":
         print "Please input ablosute path!"
