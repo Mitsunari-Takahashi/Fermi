dc2d8ad2e4fca399f38c4669e9c4063ddcb63a6b
Mitsunari Takahashi <mitsunari@ICRR-CTAs-MacBook-Pro.local>
2019-09-20 11:35:28 +0900

    AddNewVariables.cxx

---
+++ csrc/AddNewVariables.cxx
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdint.h>
+#include <getopt.h>
+#include "TMath.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TH2F.h"
+#include "TF2.h"
+#include "TTree.h"
+#include "Variable.h"
+
+using namespace std;
+using namespace TMath;
+
+int main(int argc,char *argv[])
+{
+  string strPathFileIn = (string)argv[1];
+  TFile *fileIn = new TFile(strPathFileIn.c_str(), "READ");
+  TTree *trDat =  (TTree*)fileIn->Get("MeritTuple");
+  const Int_t nEvt = trDat->GetEntries();
+  cout << trDat->GetName() << " has " << nEvt <<  " events." << endl;
+
+  //  string strPathFileFit = argv[2];
+  //  TFile* fileFit = new TFile(strPathFileFit.c_str(), "READ");
+  
+  Double_t WP8CalOnlyEnergy;
+  trDat->SetBranchAddress("WP8CalOnlyEnergy", &WP8CalOnlyEnergy);
+  Double_t Cal1MomXDir;
+  trDat->SetBranchAddress("Cal1MomXDir", &Cal1MomXDir);
+  Double_t Cal1MomYDir;
+  trDat->SetBranchAddress("Cal1MomYDir", &Cal1MomYDir);
+  Double_t Cal1MomZDir;
+  trDat->SetBranchAddress("Cal1MomZDir", &Cal1MomZDir);
+  Float_t Cal1MomXCntr;
+  trDat->SetBranchAddress("Cal1MomXCntr", &Cal1MomXCntr);
+  Float_t Cal1MomYCntr;
+  trDat->SetBranchAddress("Cal1MomYCntr", &Cal1MomYCntr);
+  Float_t Cal1MomZCntr;
+  trDat->SetBranchAddress("Cal1MomZCntr", &Cal1MomZCntr);
+  Float_t CalELayer[8];
+  for(int i=0; i<8; i++)
+    trDat->SetBranchAddress(Form("CalELayer%d", i), &CalELayer[i]);
+  Float_t CalEnergyCorr;
+  trDat->SetBranchAddress("CalEnergyCorr", &CalEnergyCorr);
+  Float_t CalEnergyRaw;
+  trDat->SetBranchAddress("CalEnergyRaw", &CalEnergyRaw);
+  Float_t Acd2TileEnergy;
+  trDat->SetBranchAddress("Acd2TileEnergy", &Acd2TileEnergy);
+  Float_t EvtJointEnergy;
+  trDat->SetBranchAddress("EvtJointEnergy", &EvtJointEnergy);
+
+  Float_t calZTop = -47.395;
+  Float_t cellVertPitch = 21.35;
+
+  string strPathFileNew = strPathFileIn.replace((int)strPathFileIn.length()-5, 5, "_newVariables.root");
+  TFile* fileNew = new TFile(strPathFileNew.c_str(), "UPDATE");
+
+  TTree* trNew = new TTree("MeritTuple", "Additional glast tuple");
+
+  Int_t Cal1MomParticleInLayer;
+  trNew->Branch('Cal1MomParticleInLayer', Cal1MomParticleInLayer, 'Cal1MomParticleInLayer/I');
+  Int_t Cal1MomParticleInLayerCor;
+  trNew->Branch('Cal1MomParticleInLayerCor', Cal1MomParticleInLayerCor, 'Cal1MomParticleInLayerCor/I');
+  Float_t Cal1MomZCrossSide840;
+  trNew->Branch('Cal1MomZCrossSide840', Cal1MomZCrossSide840, 'Cal1MomZCrossSide840/F');
+  Float_t Cal1MomZCrossSide714;
+  trNew->Branch('Cal1MomZCrossSide714', Cal1MomZCrossSide714, 'Cal1MomZCrossSide714/F');
+  Float_t Cal1MomZCrossSide840Cor;
+  trNew->Branch('Cal1MomZCrossSide840Cor', Cal1MomZCrossSide840Cor, 'Cal1MomZCrossSide840Cor/F');
+  Float_t Cal1MomZCrossSide714Cor;
+  trNew->Branch('Cal1MomZCrossSide714Cor', Cal1MomZCrossSide714Cor, 'Cal1MomZCrossSide714Cor/F');
+  Float_t CalELayerCorrInitialRatioLog;
+  trNew->Branch('CalELayerCorrInitialRatioLog', CalELayerCorrInitialRatioLog, 'CalELayerCorrInitialRatioLog/F');
+  Float_t CalELayer34afterInitialRatioLog;
+  trNew->Branch('CalELayer34afterInitialRatioLog', CalELayer34afterInitialRatioLog, 'CalELayer34afterInitialRatioLog/F');
+  Float_t CalELayerInitialRawRatioLog;
+  trNew->Branch('CalELayerInitialRawRatioLog', CalELayerInitialRawRatioLog, 'CalELayerInitialRawRatioLog/F');
+  Float_t Cal1MomParticleInLayerCor;
+  trNew->Branch('Cal1MomParticleInLayerCor', Cal1MomParticleInLayerCor, 'Cal1MomParticleInLayerCor/I');
+  Float_t CalELayerCorrInitialRatioCorLog;
+  trNew->Branch('CalELayerCorrInitialRatioCorLog', CalELayerCorrInitialRatioCorLog, 'CalELayerCorrInitialRatioCorLog/F');
+  Float_t CalELayer34afterInitialRatioCorLog;
+  trNew->Branch('CalELayer34afterInitialRatioCorLog', CalELayer34afterInitialRatioCorLog, 'CalELayer34afterInitialRatioCorLog/F');
+  Float_t CalELayerInitialRawRatioCorLog;
+  trNew->Branch('CalELayerInitialRawRatioCorLog', CalELayerInitialRawRatioCorLog, 'CalELayerInitialRawRatioCorLog/F');
+  Float_t Acd2TileEnergyRatioLog;
+  trNew->Branch('Acd2TileEnergyRatioLog', Acd2TileEnergyRatioLog, 'Acd2TileEnergyRatioLog/F');
+
+  Float_t numLayerIn = -1;
+  Float_t numLayerInCor = -1;
+  for(int iEvt=0; iEvt<nEvt; iEvt++)
+    {
+      trDat->GetEntry(iEvt);
+      if(Cal1MomXDir!=0 && Cal1MomYDir!=0)
+	{
+	  Cal1MomZCrossSide840 = (Cal1MomZCntr+Min((840-((Cal1MomXDir>0)*2-1)*Cal1MomXCntr)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir), (840-((Cal1MomYDir>0)*2-1)*Cal1MomYCntr)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir))*Cal1MomZDir);
+	  Cal1MomZCrossSide714 = (Cal1MomZCntr+Min((714-((Cal1MomXDir>0)*2-1)*Cal1MomXCntr)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir), (714-((Cal1MomYDir>0)*2-1)*Cal1MomYCntr)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir))*Cal1MomZDir);
+	  Cal1MomZCrossSide840Cor = (Cal1MomZCntrCor+Min((840-((Cal1MomXDir>0)*2-1)*Cal1MomXCntrCor)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir), (840-((Cal1MomYDir>0)*2-1)*Cal1MomYCntrCor)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir))*Cal1MomZDir);
+	  Cal1MomZCrossSide714Cor = (Cal1MomZCntrCor+Min((714-((Cal1MomXDir>0)*2-1)*Cal1MomXCntrCor)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir), (714-((Cal1MomYDir>0)*2-1)*Cal1MomYCntrCor)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir))*Cal1MomZDir);
+	}
+      else if(trDat.Cal1MomXDir!=0)
+	{
+	  Cal1MomZCrossSide840 = (Cal1MomZCntr+(840-((Cal1MomXDir>0)*2-1)*Cal1MomXCntr)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir)*Cal1MomZDir);
+	  Cal1MomZCrossSide714 = (Cal1MomZCntr+(714-((Cal1MomXDir>0)*2-1)*Cal1MomXCntr)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir)*Cal1MomZDir);
+	  Cal1MomZCrossSide840Cor = (Cal1MomZCntrCor+(840-((Cal1MomXDir>0)*2-1)*Cal1MomXCntrCor)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir)*Cal1MomZDir);
+	  Cal1MomZCrossSide714Cor = (Cal1MomZCntrCor+(714-((Cal1MomXDir>0)*2-1)*Cal1MomXCntrCor)/(((Cal1MomXDir>0)*2-1)*Cal1MomXDir)*Cal1MomZDir);
+	}
+      else if(trDat.Cal1MomYDir!=0):
+	{
+	  Cal1MomZCrossSide840 = (Cal1MomZCntr+(840-((Cal1MomYDir>0)*2-1)*Cal1MomYCntr)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir)*Cal1MomZDir);
+	  Cal1MomZCrossSide714 = (Cal1MomZCntr+(714-((Cal1MomYDir>0)*2-1)*Cal1MomYCntr)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir)*Cal1MomZDir);
+	  Cal1MomZCrossSide840Cor = (Cal1MomZCntrCor+(840-((Cal1MomYDir>0)*2-1)*Cal1MomYCntrCor)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir)*Cal1MomZDir);
+	  Cal1MomZCrossSide714Cor = (Cal1MomZCntrCor+(714-((Cal1MomYDir>0)*2-1)*Cal1MomYCntrCor)/(((Cal1MomYDir>0)*2-1)*Cal1MomYDir)*Cal1MomZDir);
+	}
+      else
+	{
+	  Cal1MomZCrossSide840 = 1000000;
+	  Cal1MomZCrossSide714 = 1000000;
+	  Cal1MomZCrossSide840Cor = 1000000;
+	  Cal1MomZCrossSide714Cor = 1000000;
+	  cout << endl;
+	  cout << "Perpendicular event!" << endl;
+	}
+      numLayerIn = Min(7.0, Max(0.0, -(Cal1MomZCrossSide714-calZTop)/cellVertPitch));
+      Cal1MomParticleInLayer = int(numLayerIn);
+
+      CalELayerCorrInitialRatioLog = (Cal1MomParticleInLayer<7)*(Log10(Max(1E-5,CalEnergyCorr)) - Log10(Max(1E-5, CalELayer[Cal1MomParticleInLayer]))); //Log10(Max(1E-5,CalEnergyCorr)) - ( ( numLayerIn<1 )*Log10(Max(1E-5, CalELayer[0])) + ( numLayerIn>=1 )*( numLayerIn<2 )*Log10(Max(1E-5, CalELayer[1])) + ( numLayerIn>=2 )*( numLayerIn<3 )*log10(max(1E-5, trDat.CalELayer2)) + ( numLayerIn>=3 )*( numLayerIn<4 )*log10(max(1E-5, trDat.CalELayer3)) + ( numLayerIn>=4 )*( numLayerIn<5 )*log10(max(1E-5, trDat.CalELayer4)) + ( numLayerIn>=5 )*( numLayerIn<6 )*log10(max(1E-5, trDat.CalELayer5)) + ( numLayerIn>=6 )*( numLayerIn<7 )*log10(max(1E-5, trDat.CalELayer6)) )
+
+      CalELayer34afterInitialRatioLog = Log10(Max(1E-5, CalELayer[Min(3,Cal1MomParticleInLayer)+4]+CalELayer[Min(3,Cal1MomParticleInLayer)+3]))-Log10(Max(1E-5, CalELayer[Min(3,Cal1MomParticleInLayer)]));
+//( numLayerIn<1 ) * ( log10(max(1E-5, trDat.CalELayer4+trDat.CalELayer3))-log10(max(1E-5, trDat.CalELayer0)) ) + ( numLayerIn>=1 ) * ( numLayerIn<2 ) * ( log10(max(1E-5, trDat.CalELayer5+trDat.CalELayer4))-log10(max(1E-5, trDat.CalELayer1)) ) + ( numLayerIn>=2 ) * ( numLayerIn<3 ) * ( log10(max(1E-5, trDat.CalELayer6+trDat.CalELayer5))-log10(max(1E-5, trDat.CalELayer2)) ) + ( numLayerIn>=3 ) * ( log10(max(1E-5, trDat.CalELayer7+trDat.CalELayer6))-log10(max(1E-5, trDat.CalELayer3)) )
+
+      CalELayerInitialRawRatioLog = (Cal1MomParticleInLayer<7) * ( Log10(Max(1E-5,CalEnergyRaw)) - Log10(Max(1E-5, CalELayer[Cal1MomParticleInLayer]) ) ); //Log10(Max(1E-5,CalEnergyRaw)) - ( ( numLayerIn<1 )*log10(max(1E-5, trDat.CalELayer0)) + ( numLayerIn>=1 )*( numLayerIn<2 )*log10(max(1E-5, trDat.CalELayer1)) + ( numLayerIn>=2 )*( numLayerIn<3 )*log10(max(1E-5, trDat.CalELayer2)) + ( numLayerIn>=3 )*( numLayerIn<4 )*log10(max(1E-5, trDat.CalELayer3)) + ( numLayerIn>=4 )*( numLayerIn<5 )*log10(max(1E-5, trDat.CalELayer4)) + ( numLayerIn>=5 )*( numLayerIn<6 )*log10(max(1E-5, trDat.CalELayer5)) + ( numLayerIn>=6 )*( numLayerIn<7 )*log10(max(1E-5, trDat.CalELayer6)) )
+
+      numLayerInCor = Min(7.0, Max(0.0, -(Cal1MomZCrossSide714Cor-calZTop)/cellVertPitch));
+      Cal1MomParticleInLayerCor = int(numLayerInCor);
+      CalELayerCorrInitialRatioCorLog = (Cal1MomParticleInLayerCor<7)*(Log10(Max(1E-5,CalEnergyCorr)) - Log10(Max(1E-5, CalELayer[Cal1MomParticleInLayerCor]))); 
+      CalELayer34afterInitialRatioCorLog = Log10(Max(1E-5, CalELayer[Min(3,Cal1MomParticleInLayerCor)+4]+CalELayer[Min(3,Cal1MomParticleInLayerCor)+3])) - Log10(Max(1E-5, CalELayer[Min(3,Cal1MomParticleInLayerCor)]));
+      CalELayerInitialRawRatioLog = (Cal1MomParticleInLayerCor<7) * ( Log10(Max(1E-5,CalEnergyRaw)) - Log10(Max(1E-5, CalELayer[Cal1MomParticleInLayerCor]) ) ); 
+
+      Acd2TileEnergyRatioLog = Log10(Max(Acd2TileEnergy/Max(10., EvtJointEnergy) * 100, 1E-5));
+
+      if(iEvt%100000==0)
+	{
+	  cout << "." << flush;
+	  if(iEvt%1000000==0)
+	    {
+	      cout << Form("\n%d%% have been filled.\n", (trNew->GetEntries()*100)/nEvt) << flush;
+	      cout << "Cal1MomParticleInLayerCor: " << Cal1MomParticleInLayerCor << endl;
+	    }
+	}
+      trNew->Fill();
+    }
+  cout << endl;
+  trNew->Write();
+  delete trNew;
+  fileNew->Close();
+  fileIn->Close();
+}
